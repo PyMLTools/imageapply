e2abdb90e439cc2ba19df0c508a0c4bd6ca5da1f
fynnsu <fynnsu@outlook.com>
2022-11-14 16:15:34 -0500

    Format code

--- imageapply/preprocess.py
+++ imageapply/preprocess.py
@@ -1,10 +1,14 @@
 # Preprocessing tools for training
 from .data import data_object
 
-def divide_into_regions(data : data_object, region_size : tuple, pad_mode : str) -> data_object:
+
+def divide_into_regions(
+    data: data_object, region_size: tuple, pad_mode: str
+) -> data_object:
     """
     Divides the data into regions of size region_size.
     """
 
-    assert len(region_size) == len(data.shape) - 1, "Region size must have same number of dimensions as data"
-
+    assert (
+        len(region_size) == len(data.shape) - 1
+    ), "Region size must have same number of dimensions as data"
